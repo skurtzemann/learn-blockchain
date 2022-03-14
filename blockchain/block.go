5fec7ef5dba3bde5ba00e195c00e1af86e07bab7
Sébastien Kurtzemann <seb@kurtzemann.fr>
2022-03-14 15:51:35 +0100

    feat(part2): refactoring by moving part1 logic into a blockchain package

---
+++ blockchain/block.go
@@ -0,0 +1,53 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	// Join current block data and previous block hash
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	// Calculate hash of info
+	hash := sha256.Sum256(info)
+	// Then assign the result to block hash
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{
+		[]byte{},
+		[]byte(data),
+		prevHash}
+	block.DeriveHash()
+	return block
+}
+
+// Genesis is the first block of the chain with a empty PrevHash
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+// Add a block into the blockchain
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
+
+// Init a BlockChain with a first Genesis block
+func InitBlockChain() *BlockChain {
+	return &BlockChain{
+		[]*Block{Genesis()},
+	}
+}
--- main.go
+++ main.go
@@ -1,71 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
 	"strconv"
+
+	"github.com/skurtzemann/learn-blockchain/blockchain"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	// Join current block data and previous block hash
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	// Calculate hash of info
-	hash := sha256.Sum256(info)
-	// Then assign the result to block hash
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{
-		[]byte{},
-		[]byte(data),
-		prevHash}
-	block.DeriveHash()
-	return block
-}
-
-// Genesis is the first block of the chain with a empty PrevHash
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-// Add a block into the blockchain
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-// Init a BlockChain with a first Genesis block
-func InitBlockChain() *BlockChain {
-	return &BlockChain{
-		[]*Block{Genesis()},
-	}
-}
-
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for i, block := range chain.blocks {
+	for i, block := range chain.Blocks {
 		fmt.Printf("[%s]\n", strconv.Itoa(i))
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 	}
-
 }
